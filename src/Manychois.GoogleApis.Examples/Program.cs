2c547f54269859610c6802b17d1a089b26f4b1e3
manychois <manychois@gmail.com>
2017-02-17 00:51:20 +1100

    Rewrite example to make it simpler

--- src/Manychois.GoogleApis.Examples/Program.cs
+++ src/Manychois.GoogleApis.Examples/Program.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Manychois.GoogleApis.AdWords.v201609;
+using Manychois.GoogleApis.OAuth2;
 using Microsoft.Extensions.Logging;
 using NLog.Extensions.Logging;
-using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Manychois.GoogleApis.Examples
 {
 	public class Program
 	{
-		static ILogger<Program> Logger;
-
 		public static void Main(string[] args)
 		{
 			NLog.LogManager.Configuration = new NLog.Config.XmlLoggingConfiguration(
@@ -20,49 +18,55 @@ namespace Manychois.GoogleApis.Examples
 			var loggerFactory = new LoggerFactory();
 			loggerFactory.AddNLog();
 
-			Logger = loggerFactory.CreateLogger<Program>();
-
-			try
-			{
-				var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory());
-				// avoid accidentially checking in sensitive data to the source control 
-				if (args.Length > 0)
-				{
-					builder.AddJsonFile(args[0]);
-				}
-				else
-				{
-					builder.AddJsonFile("appsettings.json");
-				}
-				IConfigurationRoot programConfig = builder.Build();
-
-				var program = new Program();
-				program.RunAsync(programConfig, loggerFactory).ConfigureAwait(false).GetAwaiter().GetResult();
-
-			}
-			catch (Exception ex)
-			{
-				Logger.LogError("{0}: {1}{2}{3}", ex.GetType().Name, ex.Message, Environment.NewLine, ex.StackTrace);
-			}
+			var program = new Program(loggerFactory);
+			program.RunAsync(new NetUtility(), loggerFactory).Wait();
 		}
 
-		async Task RunAsync(IConfigurationRoot programConfig, ILoggerFactory loggerFactory)
+		private readonly ILogger _logger;
+
+		public Program(ILoggerFactory loggerFactory)
 		{
-			// comment out the examples you want to skip
+			_logger = loggerFactory.CreateLogger<Program>();
+		}
+
+		async Task RunAsync(INetUtility netUtil, ILoggerFactory loggerFactory)
+		{
 			try
 			{
-				/*
-				var oauth2Examples = new OAuth2.AllExamples(programConfig, loggerFactory);
-				await oauth2Examples.RunAsync().ConfigureAwait(false);
-				*/
-				
-				var adWordsExamples = new AdWords.AllExamples(programConfig, loggerFactory);
-				await adWordsExamples.RunAsync().ConfigureAwait(false);
-				
+				var credential = InstalledAppCredential.ReadFromFile("Path of your json file downloaded from your Google API Console");
+				var oauth2Util = new OAuth2Utility(netUtil);
+				var tokenInfo = await oauth2Util.GetTokenInfoAsync(credential, "refreshToken"); // assume you have already obtained your refresh token
+
+				var adWordsApiConfig = new AdWordsApiConfig();
+				adWordsApiConfig.AccessToken = tokenInfo.AccessToken;
+				adWordsApiConfig.ClientCustomerId = "Your client customer ID";
+				adWordsApiConfig.DeveloperToken = "Developer token";
+				adWordsApiConfig.UserAgent = "Testing example"; // this and the rest of the properties are optional
+
+				// An example of using some AdWords service
+				ICampaignService campaignService = new CampaignService(adWordsApiConfig, netUtil, loggerFactory); // loggerFactory can be null
+				var selector = new Selector<CampaignServiceField>()
+					.AddFields(CampaignServiceField.Id, CampaignServiceField.Name)
+					.AddPredicate(CampaignServiceField.Status, PredicateOperator.Equals, CampaignStatus.Enabled.ToXmlValue());
+				var campaignPage = await campaignService.GetAsync(selector);
+
+				// An example of getting AdWords report
+				IReportUtility reportUtil = new ReportUtility(adWordsApiConfig, netUtil, loggerFactory); // loggerFactory can be null
+				var rptDef = new ReportDefinition();
+				rptDef.DateRangeType = ReportDefinitionDateRangeType.AllTime;
+				rptDef.DownloadFormat = DownloadFormat.GzippedCsv;
+				rptDef.ReportName = "Testing Report";
+				rptDef.ReportType = ReportDefinitionReportType.CampaignPerformanceReport;
+				rptDef.Selector = new Selector
+				{
+					Fields = new List<string>()
+				};
+				rptDef.Selector.Fields.AddRange(new string[] { "CampaignId", "CampaignName", "Impressions" });
+				var reportCsv = await reportUtil.GetContentStringAsync(rptDef);				
 			}
 			catch (Exception ex)
 			{
-				Logger.LogError("{0}: {1}{2}{3}", ex.GetType().Name, ex.Message, Environment.NewLine, ex.StackTrace);
+				_logger.LogError("{0}: {1}{2}{3}", ex.GetType().Name, ex.Message, Environment.NewLine, ex.StackTrace);
 			}
 		}
 	}
